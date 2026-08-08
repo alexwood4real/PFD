c3a092d2f5e7e52ad08d4d840079e5f9cd4d8fe8
alexwood4real <akwood6@asu.edu>
2026-08-08 16:44:02 -0700

    Integrate Data Posting on Pico
    This successfully posts the json when it is being listened to and posts an error when it is not. My computer is able to receive the latest posted json this way. Adding file reading.json as an example of how this looks when a tcp connection is enabled. this happens when running nc <pico-ip> 8080 > reading.json in the terminal. Closes #20

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,34 @@
 
 /* public crates */
 use bme280_rs::{AsyncBme280, Configuration, Oversampling, SensorMode};
-use cyw43::{NetDriver, Runner as Cyw43Runner, SpiBus, aligned_bytes};
+use core::cell::RefCell;
+use cyw43::{JoinOptions, NetDriver, Runner as Cyw43Runner, SpiBus, aligned_bytes};
 use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_net::Runner as NetRunner;
+use embassy_net::{
+    IpListenEndpoint,
+    Runner as NetRunner,
+    Stack,
+    StackResources,
+    tcp::TcpSocket,
+};
 use embassy_rp::{
     bind_interrupts,
-    dma::{Channel, InterruptHandler as DmaHandler},
+    dma::{Channel as DmaChannel, InterruptHandler as DmaHandler},
     gpio::{Level, Output},
     i2c::{Config as I2cConfig, I2c, InterruptHandler as I2cHandler},
     peripherals::{DMA_CH0, I2C0, PIO0, USB},
     pio::{InterruptHandler as PioHanlder, Pio},
     usb::{Driver as UsbDriver, InterruptHandler as UsbHandler},
 };
+use embassy_sync::{
+    blocking_mutex::{
+        raw::CriticalSectionRawMutex, 
+        Mutex},
+};
 use embassy_time::{Delay, Duration, Timer};
+use embedded_io_async::Write;
 use log::info;
 use serde_json_core;
 use static_cell::StaticCell;
@@ -41,10 +54,24 @@ use crate::calc::psychometric::SensorData;
 mod calc;
 
 /* Constants */
+const PORT: u16 = 8080;
 const BUF_SIZE: usize = 1 << 9;
 const WIFI_NETWORK: &str = env!("WIFI_SSID");
 const WIFI_PASSWORD: &str = env!("WIFI_PASSWORD");
 use {defmt_rtt as _, panic_probe as _};
+
+/* Structs */
+#[derive(Debug, Clone, Copy)]
+struct WeatherPacket {
+    data: [u8; BUF_SIZE],
+    len: usize,
+}
+
+/* Statics */
+static WEATHER_DATA: Mutex<
+    CriticalSectionRawMutex, 
+    RefCell<Option<WeatherPacket>>,
+> = Mutex::new(RefCell::new(None));
 
 /* Interrupt Handlers */
 bind_interrupts!(struct Irqs {
@@ -77,13 +104,47 @@ async fn heartbeat(mut control: cyw43::Control<'static>) -> ! {
 }
 
 #[embassy_executor::task]
+async fn logger_task(driver: UsbDriver<'static, USB>) {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
+
+#[embassy_executor::task]
 async fn net_task(mut runner: NetRunner<'static, NetDriver<'static>>) -> ! {
     runner.run().await
 }
 
 #[embassy_executor::task]
-async fn logger_task(driver: UsbDriver<'static, USB>) {
-    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+async fn tcp_server_task(stack: Stack<'static>) -> ! {
+    let mut rx_buffer = [0u8; 1024];
+    let mut tx_buffer = [0u8; 1024];
+
+    loop {
+        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+
+        info!("Waiting for connection");
+        if let Err(e) = socket.accept(IpListenEndpoint { addr: None, port: PORT }).await {
+            info!("Accept error: {:?}", e);
+            continue;
+        }
+
+        info!("client connected");
+        let pkt = WEATHER_DATA.lock(|data| {
+            *data.borrow()
+        });
+
+        if let Some(pkt) = pkt {
+           if let Err(e) = socket.write_all(&pkt.data[..pkt.len]).await {
+                info!("Write error: {:?}", e);
+            } 
+        } else {
+            info!("No weather data available yet");
+        }
+        
+        /* wait 2 sec before closing socket */
+        Timer::after(Duration::from_secs(2)).await;
+        
+        socket.close();
+    }
 }
 
 #[embassy_executor::main]
@@ -114,7 +175,7 @@ async fn main(spawner: Spawner) {
         cs,
         p.PIN_24,
         p.PIN_29,
-        Channel::new(p.DMA_CH0, Irqs),
+        DmaChannel::new(p.DMA_CH0, Irqs),
     );
 
     /* Allocate state driver */
@@ -137,11 +198,11 @@ async fn main(spawner: Spawner) {
     let net_config = embassy_net::Config::dhcpv4(Default::default());
     let seed: u64 = 0x0123_4567_89ab_cdef;
 
-    static RESOURCES: StaticCell<embassy_net::StackResources<3>> = StaticCell::new();
+    static RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
     let (stack, net_runner) = embassy_net::new(
         net_device,
         net_config,
-        RESOURCES.init(embassy_net::StackResources::new()),
+        RESOURCES.init(StackResources::new()),
         seed,
     );
 
@@ -152,7 +213,7 @@ async fn main(spawner: Spawner) {
         match control
             .join(
                 WIFI_NETWORK,
-                cyw43::JoinOptions::new(WIFI_PASSWORD.as_bytes()),
+                JoinOptions::new(WIFI_PASSWORD.as_bytes()),
             )
             .await
         {
@@ -186,6 +247,9 @@ async fn main(spawner: Spawner) {
             .await
     );
 
+    /* turn on tcp server */
+    spawner.spawn(unwrap!(tcp_server_task(stack)));
+
     /* infinite main loop */
     loop {
         /* read data from single sample */
@@ -208,6 +272,15 @@ async fn main(spawner: Spawner) {
                     if let Ok(json_str) = core::str::from_utf8(json_bytes) {
                         info!("JSON: {}", json_str);
                     }
+
+                    let pkt = WeatherPacket {
+                        data: buf,
+                        len
+                    };
+
+                    WEATHER_DATA.lock(|data| {
+                        data.borrow_mut().replace(pkt);
+                    });
                 }
                 Err(err) => info!("Serialization failed, error: {:?}", err)
             }
